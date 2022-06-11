9108554829b513c2da2623c0d967abcee7d953f9
George Torianik <htorianik@amplify.com>
2022-06-11 17:10:53 +0300

    Add sqlite_version[_info] variables to the module.

--- src/gsqlite/__init__.py
+++ src/gsqlite/__init__.py
@@ -3,9 +3,18 @@ import logging
 from .connection import connect
 
 
+def get_version() -> str:
+    conn = connect(":memory:")
+    cursor = conn.cursor()
+    cursor.execute("select sqlite_version()")
+    return cursor.fetchone()[0]
+
+
 apilevel = "2.0"
 threadsafety = 1
 paramstyle = "qmark"
+sqlite_version = get_version()
+sqlite_version_info = tuple(int(v) for v in sqlite_version.split("."))
 
 
 logging.basicConfig(level=logging.DEBUG)
